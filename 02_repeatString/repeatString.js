1963e80944c662004aeb689a30a2f1e8d4854fab
Joeri <jordan.nottebart@gmail.com>
2023-07-10 22:20:07 +0200

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    
+    let repeatedWord = "";
 
+    for (let index = 0; index < num; index++) {
+        repeatedWord = repeatedWord + string;
+    }
+
+    return repeatedWord;
 };
 
 // Do not edit below this line
