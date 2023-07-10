9f89e828e8fda213fb3b5159814dc03ab9d0a37f
Joeri <jordan.nottebart@gmail.com>
2023-07-10 22:34:03 +0200

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reverseWord = "";
 
+    for (let index = word.length - 1; index >= 0; index--) {
+        let letter = word.substr(index, 1);
+        reverseWord += letter;
+    }
+
+    return reverseWord;
 };
 
 // Do not edit below this line
