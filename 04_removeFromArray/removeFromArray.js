15fca11ad07f6a43971c0688c3cc3b19592be19b
Joeri <jordan.nottebart@gmail.com>
2023-07-10 23:17:35 +0200

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    for (let argsIndex = 0; argsIndex < theArgs.length; argsIndex++) {
+        let arg = theArgs[argsIndex];
 
+        for (let index = 0; index < array.length; index++) {
+            if (array[index] === arg) {
+                array.splice(index, 1);
+            }
+        }
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
