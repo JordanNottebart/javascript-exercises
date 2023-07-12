de8c276b50f76b06c17ad1b015b5e4aab94ca83c
Joeri <jordan.nottebart@gmail.com>
2023-07-12 22:33:23 +0200

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(beginNumber, endingNumber) {
+    let result = 0;
+    
+    if (beginNumber < 0 || endingNumber < 0) {
+        return "ERROR";
+    }
 
+    if (beginNumber > endingNumber) {
+        let helper = endingNumber;
+        endingNumber = beginNumber;
+        beginNumber = helper;
+    }
+
+    if (typeof beginNumber != "number" || typeof endingNumber != "number") {
+        return "ERROR";
+    }
+
+    for (let index = beginNumber; index <= endingNumber; index++) {
+        result += index;
+    }
+      
+    return result;
 };
 
 // Do not edit below this line
